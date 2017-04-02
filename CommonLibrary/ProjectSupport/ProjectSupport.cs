c037aaedc82a3cabd6573dc20159101295878fcd
hsl <hsl200909@163.com>
2017-04-02 13:53:48 +0800

    增加了一些项目类的基础，项目管理系统将依附一个数据库来实现

---
+++ CommonLibrary/ProjectSupport/ProjectSupport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonLibrary
+{
+    /// <summary>
+    /// 用来管理的项目类
+    /// </summary>
+    public class Project
+    {
+        /// <summary>
+        /// 项目编号
+        /// </summary>
+        public string ProjectId { get; set; } = string.Empty;
+
+
+    }
+}
